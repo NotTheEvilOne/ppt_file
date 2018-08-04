421b4260ac31b9572caec8fc81c2adf27a3475fd
NotTheEvilOne <git-NTEO@vplace.de>
2018-08-04 12:25:25 +0200

    Enhancement: Add support for additional "setup.py" commands

--- setup.py
+++ setup.py
@@ -16,12 +16,17 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 setup.py
 """
 
-from os import path
+from os import makedirs, path
 
 from distutils.core import setup
 
-from dNG.distutils.command.build_py import BuildPy
-from dNG.distutils.temporary_directory import TemporaryDirectory
+try:
+    from dNG.distutils.command.build_py import BuildPy
+    from dNG.distutils.command.sdist import Sdist
+    from dNG.distutils.temporary_directory import TemporaryDirectory
+except ImportError:
+    raise RuntimeError("'dng-builder-suite' prerequisite not matched")
+#
 
 def get_version():
     """
@@ -40,27 +45,29 @@ with TemporaryDirectory(dir = ".") as build_directory:
     BuildPy.set_build_target_path(build_directory)
     BuildPy.set_build_target_parameters(parameters)
 
-    _build_path = path.join(build_directory, "src")
+    Sdist.set_build_target_path(build_directory)
+    Sdist.set_build_target_parameters(parameters)
 
-    setup(name = "dng-file",
-          version = get_version(),
-          description = "Working with a file abstraction layer",
-          long_description = """The file.py abstraction layer provides an interface similar to FileIO with support for read, lock and write timeouts.""",
-          author = "direct Netware Group et al.",
-          author_email = "web@direct-netware.de",
-          license = "MPL2",
-          url = "https://www.direct-netware.de/redirect?py;file",
+    makedirs(path.join(build_directory, "src", "dNG"))
 
-          platforms = [ "any" ],
+    _setup = { "name": "dng-file",
+               "version": get_version(),
+               "description": "Working with a file abstraction layer",
+               "long_description": "The file.py abstraction layer provides an interface similar to FileIO with support for read, lock and write timeouts.",
+               "author": "direct Netware Group et al.",
+               "author_email": "web@direct-netware.de",
+               "license": "MPL2",
+               "url": "https://www.direct-netware.de/redirect?py;file",
 
-          setup_requires = "dng-builder-suite",
+               "platforms": [ "any" ],
 
-          package_dir = { "": _build_path },
-          packages = [ "dNG" ],
+               "packages": [ "dNG" ],
 
-          data_files = [ ( "docs", [ "LICENSE", "README" ]) ],
+               "data_files": [ ( "docs", [ "LICENSE", "README" ]) ]
+             }
 
-          # Override build_py to first run builder.py over all PAS modules
-          cmdclass = { "build_py": BuildPy }
-         )
+    # Override build_py to first run builder.py
+    _setup['cmdclass'] = { "build_py": BuildPy, "sdist": Sdist }
+
+    setup(**_setup)
 #
