27f5180b20b375d95b679fa43bd12164d568cbe5
NotTheEvilOne <git-NTEO@vplace.de>
2019-10-30 08:47:09 +0100

    Introduce "__slots__" to all classes
    (Enhancement)

--- src/dpt_file/file.py
+++ src/dpt_file/file.py
@@ -57,6 +57,22 @@ Get file objects to work with files easily.
              Mozilla Public License, v. 2.0
     """
 
+    __slots__ = [ "binary",
+                  "chmod",
+                  "file_path_name",
+                  "file_size",
+                  "_handle",
+                  "_handle_lock",
+                  "_log_handler",
+                  "readonly",
+                  "timeout_retries",
+                  "umask"
+                ]
+    """
+python.org: __slots__ reserves space for the declared variables and prevents
+the automatic creation of __dict__ and __weakref__ for each instance.
+    """
+
     def __init__(self, default_umask = None, default_chmod = None, timeout_retries = 5, log_handler = None):
         """
 Constructor __init__(File)
